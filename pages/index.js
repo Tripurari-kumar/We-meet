048af29b62a51f37f48bf1eaa7c4c1a30d432ca6
Tripurari kumar <tripurari.kumar@lennar.com>
2023-12-15 22:31:03 +0530

    added peer setup

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { useSocket } from '@/context/socket';
+import usePeer from '@/hooks/usePeer';
 import { useEffect } from 'react';
 
 export default function Home() {
   const socket = useSocket();
+  usePeer();
 
   useEffect(() => {
     socket?.on('connect', () => {
